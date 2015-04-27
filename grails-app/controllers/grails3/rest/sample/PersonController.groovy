b6c8b2ef9ec9dea1fc8f3df786e695722d927d7a
Fatima Casau <fatimacasau@gmail.com>
2015-04-27 02:28:48 +0200

    Custom RESTful controller

--- grails-app/controllers/grails3/rest/sample/PersonController.groovy
+++ grails-app/controllers/grails3/rest/sample/PersonController.groovy
@@ -1,12 +1,24 @@
 package grails3.rest.sample
 
-import grails.rest.RestfulController
+import grails.transaction.Transactional
 
-class PersonController extends RestfulController{
+@Transactional(readOnly = true)
+class PersonController {
 
     static responseFormats = ['json', 'xml']
 
-    PersonController(){
-        super(Person)
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond Person.list(params), model:[personCount: Person.count()]
     }
+
+    def show(Person person) {
+        if(person == null) {
+            render status:404
+        }
+        else {
+            respond person
+        }
+    }
+
 }
